c4f43a62e36dec1a7bc349d52efcbb51967bc384
AMoudretsov <AMoudretsov@gmail.com>
2021-05-09 13:33:48 +0300

    Added tests for:   - Auto property intializers.   - Index intializers.

---
+++ CSharp6Features.Tests/AutoPropertyInitializers/AutoPropertyContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp6Features.Tests.AutoPropertyInitializers
+{
+    public class AutoPropertyContainer
+    {
+        public string ReadOnlyAutoProperty { get; } = nameof(ReadOnlyAutoProperty);
+
+        public string AutoProperty { get; set; } = nameof(AutoProperty);
+
+        public string NotInitializedReadOnlyAutoProperty { get; }
+
+        public string NotInitializedAutoProperty { get; set; }
+    }
+}
---
+++ CSharp6Features.Tests/AutoPropertyInitializers/AutoPropertyInitializersTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace CSharp6Features.Tests.AutoPropertyInitializers
+{
+    public class AutoPropertyInitializersTests
+    {
+        [Fact]
+        public void ReadOnlyAutoPropertyIsInitialized()
+        {
+            var actual = new AutoPropertyContainer().ReadOnlyAutoProperty;
+
+            Assert.Equal(nameof(AutoPropertyContainer.ReadOnlyAutoProperty), actual);
+        }
+
+
+        [Fact]
+        public void AutoPropertyIsInitialized()
+        {
+            var actual = new AutoPropertyContainer().AutoProperty;
+
+            Assert.Equal(nameof(AutoPropertyContainer.AutoProperty), actual);
+        }
+
+        [Fact]
+        public void NotInitializedReadOnlyAutoPropertyIsNull()
+        {
+            var actual = new AutoPropertyContainer().NotInitializedReadOnlyAutoProperty;
+
+            Assert.Null(actual);
+        }
+
+
+        [Fact]
+        public void NotInitializedAutoPropertyIsNull()
+        {
+            var actual = new AutoPropertyContainer().NotInitializedAutoProperty;
+
+            Assert.Null(actual);
+        }
+    }
+}
