c432515ce45c0bd1a0d4d77cdd76ecdc2dd2da58
AMoudretsov <AMoudretsov@gmail.com>
2021-05-09 18:19:05 +0300

    Added tests for expression bodied methods and string interpolation.

---
+++ CSharp6Features.Tests/ExpressionBodiedMethods/ExpressionBodiedMethodsTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace CSharp6Features.Tests.ExpressionBodiedMethods
+{
+    public class ExpressionBodiedMethodsTests
+    {
+        private string Echo(string value) => value;
+
+        [Fact]
+        public void ExpressionBodiedMethodReturnsExpressionResult()
+        {
+            Assert.Equal("Test value", Echo("Test value"));
+        }
+    }
+}
---
+++ CSharp6Features.Tests/Interpolation/InterpolationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace CSharp6Features.Tests.Interpolation
+{
+    public class InterpolationTests
+    {
+        [Fact]
+        public void InterpolatedStringInludesResultsOfInterpolationExpressions()
+        {
+            var name = "Saturn";
+            var number = 6;
+
+            Assert.Equal(
+                "Saturn is the 6 planet from the sun.",
+                $"{name} is the {number} planet from the sun.");
+        }
+
+        [Fact]
+        public void AddingFormatStringReturnsFormattedResultOfInterpolationExpression()
+        {
+            var date = new DateTime(2020, 5, 15, 10, 30, 55);
+
+            Assert.Equal("20200515 10:30:55", $"{date:yyyyMMdd hh:mm:ss}");
+        }
+
+        [Fact]
+        public void AddingAlignmentReturnsFormattedResultOfInterpolationExpression()
+        {
+            var name = "Saturn";
+            var number = 6;
+
+            Assert.Equal("Saturn             6", $"{name,-10}{number,10}");
+        }
+    }
+}
