769bf1321537c2698c6403374e174a52686e8121
AMoudretsov <AMoudretsov@gmail.com>
2021-05-13 00:33:55 +0300

    Added tests for:   - Default literal.   - Inferring of Tuple property names.   - Pattern matching on generic parameter type.

---
+++ CSharp71Features.Tests/PatternMatching/PatternMatchingTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using Xunit;
+
+namespace CSharp71Features.Tests.PatternMatching
+{
+    public class PatternMatchingTests
+    {
+        [Fact]
+        public void PatternMatchingCanBeUsedOnGenericTypeParameter()
+        {
+            string toString<T>(T value)
+            {
+                switch (value)
+                {
+                    case int intValue:
+                        return intValue.ToString(NumberFormatInfo.InvariantInfo);
+                    case string stringValue:
+                        return stringValue;
+                    case null:
+                        return "Null";
+                    default:
+                        return "Unknown";
+                }
+            };
+
+            Assert.Equal("10", toString(10));
+            Assert.Equal("Test value", toString("Test value"));
+            Assert.Equal("Null", toString<object>(null));
+            Assert.Equal("Unknown", toString(new object()));
+        }
+    }
+}
---
+++ CSharp71Features.Tests/TuplePropertyNames/TuplePropertyNamesTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace CSharp71Features.Tests.TuplePropertyNames
+{
+    public class TuplePropertyNamesTests
+    {
+        [Fact]
+        public void TuplePropertyNamesAreInferredFromLocalVariables()
+        {
+            var name = "Test name";
+            var number = 10;
+
+            var actual = (name, number);
+
+            Assert.Equal(name, actual.name);
+            Assert.Equal(number, actual.number);
+        }
+
+        [Fact]
+        public void TuplePropertyNamesAreInferredFromObjectProperties()
+        {
+            var entity = new
+            {
+                Name = "Test name",
+                Number = 10,
+                Description = "Test description"
+            };
+
+            var actual = (entity.Name, entity.Number);
+
+            Assert.Equal(entity.Name, actual.Name);
+            Assert.Equal(entity.Number, actual.Number);
+        }
+    }
+}
