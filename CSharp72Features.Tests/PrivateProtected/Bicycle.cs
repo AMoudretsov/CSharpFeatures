470d26630a49320409de3d41f8a0b01a34680396
AMoudretsov <AMoudretsov@gmail.com>
2021-05-25 00:35:08 +0300

    Added tests for private protected members, non trailing named args and leading underscore delimiters.

---
+++ CSharp72Features.Tests/PrivateProtected/Bicycle.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp72Features.Tests.PrivateProtected
+{
+    public class Bicycle : Vehicle
+    {
+        public Bicycle() : base(2)
+        {
+        }
+
+        public int HowManyWheels() => _wheels;
+    }
+}
---
+++ CSharp72Features.Tests/PrivateProtected/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp72Features.Tests.PrivateProtected
+{
+    public class Vehicle
+    {
+        private protected int _wheels;
+
+        public Vehicle(int wheels)
+        {
+            _wheels = wheels;
+        }
+    }
+}
